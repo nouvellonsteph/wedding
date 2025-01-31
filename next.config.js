5ceb3b1cd2db1336a7cd64a0b3f020bebc3b41f3
Stephane Nouvellon <stephane@cloudflare.com>
2025-01-31 10:22:11 +0100

    config: update next.config.js to include experimental settings and change distDir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  distDir: 'build', 
   images: {
     unoptimized: true,
   },
